37636975816420ea41bf12de0407a55b89b9a442
Shishir Subedi <shishirsubedi41@gmail.com>
2023-01-11 01:55:01 +0545

    fixed admin user creation on startup

--- app/user/apps.py
+++ app/user/apps.py
@@ -6,12 +6,15 @@ class UserConfig(AppConfig):
 
     def ready(self):
         from user.models import User
-        admin_user = User.objects.filter(is_superuser=True)
-        if not admin_user.exists():
-            User.objects.create_user(
-                username="admin",
-                email="admin@admin.com",
-                password="ubuntu",
-                is_staff=True,
-                is_superuser=True
-            )
+        try:
+            admin_user = User.objects.filter(is_superuser=True)
+            if not admin_user.exists():
+                User.objects.create_user(
+                    username="admin",
+                    email="admin@admin.com",
+                    password="ubuntu",
+                    is_staff=True,
+                    is_superuser=True
+                )
+        except:
+            print("User table not ready yet!")
